5a0abbfffde07acd30ecbfc84ccb383680bde8a5
Michael Douglas <michaelneves92@gmail.com>
2020-11-14 18:24:47 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd34200ed0655db8263db5e2e7fcdf049'),
+    },
+  },
+});
